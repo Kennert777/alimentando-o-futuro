cf6b5b0dcb201654fcf882d53302b129af9675a7
Kennert777 <rm94720@estudante.fieb.edu.br>
2026-03-03 11:03:12 +0000

    v2026

--- frontend/src/pages/admin/AdminRegister.jsx
+++ frontend/src/pages/admin/AdminRegister.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import api from '../../services/api';
+import { api } from '../../config/api';
 
 export default function AdminRegister() {
     const [formData, setFormData] = useState({
